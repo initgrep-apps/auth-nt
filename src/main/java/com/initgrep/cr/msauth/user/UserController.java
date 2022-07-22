41ffb7cd8eb4b62e9c445a20fb95159886742383
irshadahmad.shaikh@dxc.com <irshadahmad.shaikh@dxc.com>
2022-07-22 15:00:58 +0530

    first commit

---
+++ src/main/java/com/initgrep/cr/msauth/user/UserController.java
@@ -0,0 +1,15 @@
+package com.initgrep.cr.msauth.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello from the other side";
+    }
+}
