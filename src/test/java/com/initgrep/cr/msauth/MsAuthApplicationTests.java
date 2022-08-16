20dd69e0331001655a0098488a7c5adb2b319bf4
irshad <irshadahmad.shaikh@dxc.com>
2022-08-16 21:52:22 +0530

    chore:seperate database for test and build added

--- src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
+++ src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
@@ -2,8 +2,12 @@ package com.initgrep.cr.msauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
+@TestPropertySource("classpath:/application-test.properties")
 class MsAuthApplicationTests {
 
 	@Test
