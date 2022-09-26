276ed04ff323ac0a4ae679fe6578ea23238d63b0
irshad <irshadahmad.shaikh@dxc.com>
2022-09-26 20:09:50 +0530

    validations :+1:  email added

---
+++ src/main/java/com/initgrep/cr/msauth/auth/constants/ValidationConstants.java
@@ -0,0 +1,10 @@
+package com.initgrep.cr.msauth.auth.constants;
+
+public final class ValidationConstants {
+    public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    public static final String PHONE_PATTERN = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$";
+
+    private ValidationConstants(){
+
+    }
+}
