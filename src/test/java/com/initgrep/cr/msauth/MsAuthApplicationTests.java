9a21eb47272895c78e0a596e40de0fa75c38141a
irshad <irshadahmad.shaikh@dxc.com>
2022-10-01 22:10:11 +0530

    adding checks

--- src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
+++ src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles(profiles = "test")
 class MsAuthApplicationTests {
 
 	@Test
