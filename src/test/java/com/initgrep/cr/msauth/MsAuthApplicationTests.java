849817363331d9233a8ee54e8b0f4131e1072ed4
irshad <irshadahmad.shaikh@dxc.com>
2022-10-01 21:50:43 +0530

    feat/CSP-166368780-ci-cd-via-github-actions

--- src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
+++ src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-
+//
 @SpringBootTest
-@ActiveProfiles(profiles = "test")
+@ActiveProfiles("test")
 class MsAuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
