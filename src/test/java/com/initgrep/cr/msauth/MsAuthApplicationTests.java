73a07c45198da4ec411f51fc2a6a4f98d0a0f258
irshad ahmad sheikh <irshsheik@gmail.com>
2022-10-08 20:23:54 +0530

    added test properties for integration test

--- src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
+++ src/test/java/com/initgrep/cr/msauth/MsAuthApplicationTests.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-
+//
 //@SpringBootTest
+//@ActiveProfiles("test")
 class MsAuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
