8ce457a7ae5d76edc9d11337b4545bfd0fed627c
irshad <irshadahmad.shaikh@dxc.com>
2022-10-13 19:42:53 +0530

    feat(CSP-166342794): Register and login validations #1

--- src/main/java/com/initgrep/cr/msauth/auth/constants/ValidationConstants.java
+++ src/main/java/com/initgrep/cr/msauth/auth/constants/ValidationConstants.java
@@ -2,9 +2,13 @@ package com.initgrep.cr.msauth.auth.constants;
 
 public final class ValidationConstants {
     public static final String EMAIL_PATTERN = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    public static final String EMAIL_INVALID = "Email is not valid";
     public static final String PHONE_PATTERN = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$";
+    public static final String PHONE_INVALID = "Phone Number is not valid";
+    public static final String TOKEN_INVALID = "refreshToken is not valid";
 
-    private ValidationConstants(){
+
+    private ValidationConstants() {
 
     }
 }
